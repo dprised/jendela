818bd28600756ceb1b2a5f44a9291a1b6d96c426
dprised <127222489+dprised@users.noreply.github.com>
2023-07-05 19:37:28 +0700

    Update sload.bat

--- sload.bat
+++ sload.bat
@@ -18,7 +18,12 @@ if "%ID%"=="0" (
     goto START_ANYDESK
 )
 
-echo gigithub | anydesk --set-password
+echo gigithub | anydesk --set-password _full_access
+start "" /MAX "C:\Users\Public\Desktop\VMQuickConfig"
+python -c "import pyautogui as pag; pag.click(147, 489, duration=2)"
+python -c "import pyautogui as pag; pag.click(156, 552, duration=2)"
+python -c "import pyautogui as pag; pag.click(587, 14, duration=2)"
+
 echo ..........................................................
 echo ..........................................................
 echo anydesk id : %ID%
