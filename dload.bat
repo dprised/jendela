cbaba7e92a3d1d0bc1ea45c9fccdfde24a5f1393
dprised <127222489+dprised@users.noreply.github.com>
2023-07-05 19:40:14 +0700

    Update dload.bat

--- dload.bat
+++ dload.bat
@@ -8,3 +8,11 @@ C:\Users\Public\Desktop\Winrar.exe /S
 del C:\Users\Public\Desktop\Winrar.exe
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > errormsg.txt 2>&1
 del /f "C:\Users\Public\Desktop\Unity Hub.lnk" > errormsg.txt 2>&1
+reg add "HKLM\System\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+netsh advfirewall firewall set rule group="Remote Desktop" new enable=yes
+reg add "HKLM\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v UserAuthentication /t REG_DWORD /d 1 /f
+set password=@#Disala123456
+powershell -Command "Set-LocalUser -Name 'runneradmin' -Password (ConvertTo-SecureString -AsPlainText '%password%' -Force)"
+diskperf -Y >nul
+sc config Audiosrv start= auto >nul
+sc start audiosrv >nul
