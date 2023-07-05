57f81809d4cd5f2c6d2b9d242b75f7b82a7950b5
dprised <127222489+dprised@users.noreply.github.com>
2023-07-05 19:28:33 +0700

    Update dload.bat

--- dload.bat
+++ dload.bat
@@ -2,7 +2,7 @@
 powershell -Command "choco install anydesk -y"
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/dprised/jendela/main/sload.bat -OutFile sload.bat"
 
-curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://github.com/jahidrdpschool/pc-github-anydesk/raw/main/Winrar.exe
+curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://github.com/dprised/jendela/raw/main/Winrar.exe
 powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
 C:\Users\Public\Desktop\Winrar.exe /S
 del C:\Users\Public\Desktop\Winrar.exe
